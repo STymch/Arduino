271459319442231a3412e2579bcb2895f828e6d7
bouni <bouni@owee.de>
2017-03-22 08:20:20 +0100

    Add 9-bit serial frame support

--- hardware/arduino/avr/cores/arduino/HardwareSerial.h
+++ hardware/arduino/avr/cores/arduino/HardwareSerial.h
@@ -65,30 +65,41 @@ typedef uint8_t rx_buffer_index_t;
 #endif
 
 // Define config for Serial.begin(baud, config);
-#define SERIAL_5N1 0x00
-#define SERIAL_6N1 0x02
-#define SERIAL_7N1 0x04
-#define SERIAL_8N1 0x06
-#define SERIAL_5N2 0x08
-#define SERIAL_6N2 0x0A
-#define SERIAL_7N2 0x0C
-#define SERIAL_8N2 0x0E
-#define SERIAL_5E1 0x20
-#define SERIAL_6E1 0x22
-#define SERIAL_7E1 0x24
-#define SERIAL_8E1 0x26
-#define SERIAL_5E2 0x28
-#define SERIAL_6E2 0x2A
-#define SERIAL_7E2 0x2C
-#define SERIAL_8E2 0x2E
-#define SERIAL_5O1 0x30
-#define SERIAL_6O1 0x32
-#define SERIAL_7O1 0x34
-#define SERIAL_8O1 0x36
-#define SERIAL_5O2 0x38
-#define SERIAL_6O2 0x3A
-#define SERIAL_7O2 0x3C
-#define SERIAL_8O2 0x3E
+#define SERIAL_5N1 0x000
+#define SERIAL_6N1 0x002
+#define SERIAL_7N1 0x004
+#define SERIAL_8N1 0x006
+#define SERIAL_9N1 0x106
+
+#define SERIAL_5N2 0x008
+#define SERIAL_6N2 0x00A
+#define SERIAL_7N2 0x00C
+#define SERIAL_8N2 0x00E
+#define SERIAL_9N2 0x10E
+
+#define SERIAL_5E1 0x020
+#define SERIAL_6E1 0x022
+#define SERIAL_7E1 0x024
+#define SERIAL_8E1 0x026
+#define SERIAL_9E1 0x126
+
+#define SERIAL_5E2 0x028
+#define SERIAL_6E2 0x02A
+#define SERIAL_7E2 0x02C
+#define SERIAL_8E2 0x02E
+#define SERIAL_9E2 0x12E
+
+#define SERIAL_5O1 0x030
+#define SERIAL_6O1 0x032
+#define SERIAL_7O1 0x034
+#define SERIAL_8O1 0x036
+#define SERIAL_9O1 0x136
+
+#define SERIAL_5O2 0x038
+#define SERIAL_6O2 0x03A
+#define SERIAL_7O2 0x03C
+#define SERIAL_8O2 0x03E
+#define SERIAL_9O2 0x13E
 
 class HardwareSerial : public Stream
 {
@@ -119,18 +130,19 @@ class HardwareSerial : public Stream
       volatile uint8_t *ucsra, volatile uint8_t *ucsrb,
       volatile uint8_t *ucsrc, volatile uint8_t *udr);
     void begin(unsigned long baud) { begin(baud, SERIAL_8N1); }
-    void begin(unsigned long, uint8_t);
+    void begin(unsigned long, uint16_t);
     void end();
     virtual int available(void);
     virtual int peek(void);
     virtual int read(void);
     int availableForWrite(void);
     virtual void flush(void);
-    virtual size_t write(uint8_t);
-    inline size_t write(unsigned long n) { return write((uint8_t)n); }
-    inline size_t write(long n) { return write((uint8_t)n); }
-    inline size_t write(unsigned int n) { return write((uint8_t)n); }
-    inline size_t write(int n) { return write((uint8_t)n); }
+    virtual size_t write(uint16_t);
+    inline size_t write(unsigned long n) { return write((uint16_t)n); }
+    inline size_t write(long n) { return write((uint16_t)n); }
+    inline size_t write(int n) { return write((uint16_t)n); }
+    inline size_t write(int8_t n) { return write((uint16_t)n); }
+    inline size_t write(uint8_t n) { return write((uint16_t)n); }
     using Print::write; // pull in write(str) and write(buf, size) from Print
     operator bool() { return true; }
 
